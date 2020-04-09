7f4e8af8e173c4c6c9438d74b4ef86814e2ce6b8
Super <38338700+superpowers04@users.noreply.github.com>
2020-04-09 04:53:01 -0400

    Update QOL.plugin.js

--- QOL.plugin.js
+++ QOL.plugin.js
@@ -1,66 +1,58 @@
 //META{"name":"QualityofLifehacksforDiscord","website":"https://github.com/superpowers04/BD-plugins/","source":"https://raw.githubusercontent.com/superpowers04/BD-plugins/master/QOL.plugin.js"}*//
 
-class QualityofLifehacksforDiscord {
+var QualityofLifehacksforDiscord = (_ => {
+    return class QualityofLifehacksforDiscord {
+        getName() { return "Quality of Life hacks for Discord"; }
+        getDescription() { return "Removes some annoyances from discord's context menu"; }
+        getVersion() { return "0.1"; }
+        getAuthor() { return "Superpowers04"; }
+        constructor () {
+        }
+        load() {}
 
-    getName() { return "Quality of Life hacks for Discord"; }
-    getDescription() { return "Removes some annoyances from discord's context menu"; }
-    getVersion() { return "0.2"; }
-    getAuthor() { return "Superpowers04"; }
-    getChanges() {
-        return {
-
-        };
-    }
-
-    load() {}
-
-    start() {
-        document.addEventListener("contextmenu", this.contextEvent = e => this.onContextMenu(e));
-    }
-
-
-
-
-    onContextMenu(e) {
-        var contexserc = "div.da-item"
-        var contexarry = $( contexserc )
-        var contexarryLength = contexarry.Length
-        for (var i = 0; i < 4; i++) {
-            
-            if (contexarry[i].innerText == "Call") {
-                $( contexserc )[i].outerHTML = "";
-                // $( contexserc )[i].clientHeight = 0;
+        start() {
+            document.addEventListener("contextmenu", this.contextEvent = e => this.onContextMenu(e));
+        }
+        onContextMenu(e) {
+            var contexserc = "div.da-item"
+            var contexarry = $( contexserc )
+            var contexarryLength = contexarry.Length
+            for (var i = 0; i < 4; i++) {
                 
-            };
-            if (contexarry[i].innerText == "Profile") {
-                if (contexarry[1].innerText == "Mark As Read") {
+                if (contexarry[i].innerText == "Call") {
                     $( contexserc )[i].outerHTML = "";
+                    // $( contexserc )[i].clientHeight = 0;
+                    
+                };
+                if (contexarry[i].innerText == "Profile") {
+                    if (contexarry[1].innerText == "Mark As Read") {
+                        $( contexserc )[i].outerHTML = "";
+                }
+                    
+                };
             }
-                
+            var contexserc = "div.da-disabled"
+            var contexarry = $( contexserc )
+            var contexarryLength = contexarry.Length
+            for (var i = 0; i < 4; i++) {
+                $( contexserc )[i].outerHTML = "";
+            }
+            var contexarryLength = $( "div.da-itemSubMenu" ).Length
+
+            if ($( "div.da-itemSubMenu" )[0].innerText == "Invite to Server") {
+                $( "div.da-itemSubMenu" )[0].outerHTML = "";
+                    
             };
+            if ($( "div.da-itemSubMenu" )[1].innerText == "Invite to Server") {
+                $( "div.da-itemSubMenu" )[1].outerHTML = "";
+                    
+            };
+                // if (contexarry[i].innerHTML()) return;
         }
-        var contexserc = "div.da-disabled"
-        var contexarry = $( contexserc )
-        var contexarryLength = contexarry.Length
-        for (var i = 0; i < 4; i++) {
-            $( contexserc )[i].outerHTML = "";
+
+        stop() {
+            
+            document.removeEventListener("contextmenu", this.contextEvent);
         }
-        var contexarryLength = $( "div.da-itemSubMenu" ).Length
-
-        if ($( "div.da-itemSubMenu" )[0].innerText == "Invite to Server") {
-            $( "div.da-itemSubMenu" )[0].outerHTML = "";
-                
-        };
-        if ($( "div.da-itemSubMenu" )[1].innerText == "Invite to Server") {
-            $( "div.da-itemSubMenu" )[1].outerHTML = "";
-                
-        };
-            // if (contexarry[i].innerHTML()) return;
     }
-
-    stop() {
-        
-        document.removeEventListener("contextmenu", this.contextEvent);
-    }
-
-}
+})();
