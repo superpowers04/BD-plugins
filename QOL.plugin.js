1045e07e4bff7fe002acb36e7392a6b282dc8d24
Super <38338700+superpowers04@users.noreply.github.com>
2020-04-09 11:59:41 -0400

    Fix plugin to work without Normalise Classes

--- QOL.plugin.js
+++ QOL.plugin.js
@@ -4,7 +4,7 @@ var QualityofLifehacksforDiscord = (_ => {
     return class QualityofLifehacksforDiscord {
         getName() { return "Quality of Life hacks for Discord"; }
         getDescription() { return "Removes some annoyances from discord's context menu"; }
-        getVersion() { return "0.1.1"; }
+        getVersion() { return "0.3"; }
         getAuthor() { return "Superpowers04"; }
         constructor () {
         }
@@ -14,15 +14,13 @@ var QualityofLifehacksforDiscord = (_ => {
             document.addEventListener("contextmenu", this.contextEvent = e => this.onContextMenu(e));
         }
         onContextMenu(e) {
-            var contexserc = "div.da-item"
+            var contexserc = "div.item-1Yvehc" //div.da-item
             var contexarry = $( contexserc )
             var contexarryLength = contexarry.Length
             for (var i = 0; i < 4; i++) {
                 
                 if (contexarry[i].innerText == "Call") {
-                    $( contexserc )[i].outerHTML = "";
-                    // $( contexserc )[i].clientHeight = 0;
-                    
+                    $( contexserc )[i].outerHTML = "";  
                 };
                 if (contexarry[i].innerText == "Profile") {
                     if (contexarry[1].innerText == "Mark As Read") {
@@ -34,19 +32,21 @@ var QualityofLifehacksforDiscord = (_ => {
             var contexserc = "div.da-disabled"
             var contexarry = $( contexserc )
             var contexarryLength = contexarry.Length
-            for (var i = 0; i < 4; i++) {
+            for (var i = 0; i < contexarry.Length; i++) {
                 $( contexserc )[i].outerHTML = "";
-            }
+            }        
+            var contexserc = "div.itemSubMenu-1vN_Yn" //da-itemSubMenu
+            var contexarry = $( contexserc )
             var contexarryLength = $( "div.da-itemSubMenu" ).Length
 
-            if ($( "div.da-itemSubMenu" )[0].innerText == "Invite to Server") {
-                $( "div.da-itemSubMenu" )[0].outerHTML = "";
+            if ($( contexserc )[0].outerText == "Invite to Server") {
+                $( contexserc )[0].outerHTML = "";
                     
             };
-            if ($( "div.da-itemSubMenu" )[1].innerText == "Invite to Server") {
-                $( "div.da-itemSubMenu" )[1].outerHTML = "";
+            // if ($( contexserc )[1].outerText == "Invite to Server") {
+            //     $( contexserc )[1].outerHTML = "";
                     
-            };
+            // };
                 // if (contexarry[i].innerHTML()) return;
         }
 
