16ff2488a8e2bfb9d3868c74094ea459d91d750c
Super <38338700+superpowers04@users.noreply.github.com>
2020-04-14 16:25:11 -0400

    Added keys to ignore
    This now ignores most modifiers, Escape and all of the function keys

--- Typingsound.plugin.js
+++ Typingsound.plugin.js
@@ -3,14 +3,20 @@ var typingSoundPlugin = (_ => {
     return class typingSoundPlugin {
         getName() { return "typingSoundPlugin"; }
         getDescription() { return "Makes some sounds when typing"; }
-        getVersion() { return "0.4"; }
+        getVersion() { return "0.5"; }
         getAuthor() { return "Superpowers04, Recreation of Jiik's old one"; }
 
         constructor () { this.changelog = {};}
-        load() {document.addEventListener("keydown", this.contextEvent = e => this.onkeydown(e));}
+        load() {document.addEventListener("keydown", this.contextEvent = e => this.onkeydown(e));     }
 
         start() {}
+
         onkeydown(e){
+        	var returncode = true;
+    		var disabledkeys = [27,17,18,9,112,113,114,115,116,117,118,119,120,121,122,123]; // You can add custom keys to this array, https://keycode.info/ can be used to figure out what key corrasponds to which keycode under 'event.which'
+    		for (var i = 0; i < disabledkeys.length; i++) {
+    			if (e.keyCode == disabledkeys[i]) return;
+    		}
        		if(e.keyCode == 8) {
        			//Change this to anything for backspace
 	       		var tssound = new Audio();
