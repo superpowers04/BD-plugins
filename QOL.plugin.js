1c505c5e46dd75f12e3226e668d50974d0244b29
Super <38338700+superpowers04@users.noreply.github.com>
2020-05-24 23:23:47 -0400

    Adjust to use Discord's new ID's for context menu options

--- QOL.plugin.js
+++ QOL.plugin.js
@@ -1,10 +1,9 @@
 //META{"name":"QualityofLifehacksforDiscord","website":"https://github.com/superpowers04/BD-plugins/","source":"https://raw.githubusercontent.com/superpowers04/BD-plugins/master/QOL.plugin.js"}*//
-
 var QualityofLifehacksforDiscord = (_ => {
     return class QualityofLifehacksforDiscord {
         getName() { return "Quality of Life hacks for Discord"; }
         getDescription() { return "Removes some annoyances from discord's context menu"; }
-        getVersion() { return "0.4"; }
+        getVersion() { return "0.5"; }
         getAuthor() { return "Superpowers04"; }
         constructor () {
         }
@@ -14,33 +13,27 @@ var QualityofLifehacksforDiscord = (_ => {
             document.addEventListener("contextmenu", this.contextEvent = e => this.onContextMenu(e));
         }
         onContextMenu(e) {
-             //da-itemSubMenu
-            var contexitem = "div.item-1Yvehc" //div.da-item
-            // var contexitemarry = $( contexitem )
+            var contexitem = "div.item-1Yvehc"
             
             var contexsubm = "div.itemSubMenu-1vN_Yn"
             var contexsubmarry = $( contexsubm )
-            // var contexitemarryLength = contexitemarry.Length
-            // var contexsubmarryLength = $( contexsubm ).Length
-
             //Remove Call button
             for (var i = 0; i < $(contexitem).length; i++) {
                 if ( $( contexitem )[i]) {
-                    if ($( contexitem )[i].innerText == "Call") {
+                    if ($( contexitem )[i].id == "Call" || $( contexitem )[i].id == "通話" ) {
                         $( contexitem )[i].outerHTML = "";  
                         i--
                     };
                 };
             }   
             //Remove Invite to Server dropdown
-            if ($( contexsubm )[0].outerText == "Invite to Server") {
+            if ($( contexsubm )[0].id == "user-context-invite-to-server") {
                 $( contexsubm )[0].outerHTML = "";
                     
             };
         }
 
         stop() {
-            
             document.removeEventListener("contextmenu", this.contextEvent);
         }
     }
